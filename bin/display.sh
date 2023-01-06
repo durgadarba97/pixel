59e269cd141dec25f7ae864b409082c232a57ad5
DurgaDarba <ddarba@gmail.com>
2023-01-06 15:09:15 -0600

    pathing issue

--- bin/display.sh
+++ bin/display.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "displaying stream file $1"
-sudo /home/spliff/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-brightness=75 --led-gpio-mapping=adafruit-hat-pwm /home/spliff/PixelBoard/output/streams/$1.stream
+sudo /home/spliff/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-brightness=75 --led-gpio-mapping=adafruit-hat-pwm /home/spliff/pixel/output/streams/$1.stream
 exit 1
