57afc02a9ce6a15599377304843e2e3117ae4c80
DurgaDarba <ddarba@gmail.com>
2023-01-06 15:32:44 -0600

    forgot again

--- bin/to_stream.sh
+++ bin/to_stream.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 echo "outputing script to stream file:$1"
-sudo /home/spliff/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm /home/spliff/PixelBoard/output/gifs/$1 -O/home/spliff/pixel/output/streams/$1.stream
+sudo /home/spliff/rpi-rgb-led-matrix/utils/led-image-viewer --led-rows=64 --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm /home/spliff/pixel/output/gifs/$1 -O/home/spliff/pixel/output/streams/$1.stream
 exit 1
